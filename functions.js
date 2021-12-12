f73e19c2a29e5e4d55ebf07c53da105a5420fa69
MErtanM <mertan1971@gmail.com>
2021-12-12 23:19:50 +0300

    function is complate

--- functions.js
+++ functions.js
@@ -33,8 +33,9 @@ const myCar =carFactory("mercedes", "opel")
 console.log(myCar)
 */
 
-// funtion tanimlama yontemi
-// 1 funtion declaration
+// ----------funtion tanimlama yontemi
+
+// 1 ----------funtion declaration
 
 function calcAge(birthyear){
     
@@ -46,10 +47,77 @@ function calcAge(birthyear){
 console.log(calcAge(1997))
 
 
-// 2 Function Expression code
+// 2 ----Function Expression code
 
 const addition = function(a, b){
         return a + b        
 }
 const total =addition(1965, 1612775)
 console.log(total)
+
+// 3 -------Arrow Function
+
+const calcAge2 = birthYear => 2021 -birthYear
+console.log(calcAge(2000))
+
+const canDrive = birthYear =>{
+        const age = 2021 - birthYear
+        const allowed = 18 - age
+        return `You can drive ${allowed} year later`
+
+}
+const result = canDrive(2006)
+console.log(result)
+
+function hipotenus (a, b){
+const firstValue = exponentValue(a) 
+const secondValue = exponentValue(b) 
+const result = (firstValue + secondValue) ** (0.5)
+return result
+        
+}
+function exponentValue(value){
+        return value ** 2    // degerin 2. kuvveti(karesi vb)
+}
+const hipoResult = hipotenus (7, 24)
+console.log(hipoResult)
+
+/* 
+
+//--------- ortalama alma fonksiyonu yaziniz
+// ortalamalari karsilastirma fonksiyonu yaziniz
+data dolphin score 96 108 89
+function avrDolp(a, b, c) {
+
+        const dolpAvr = (a+b+c)/3
+        return dolpAvr
+}
+console.log(avrDolp(96,108,89));
+        function avrKoal(a, b, c){
+                const KoalAvr =(a+b+c)/3
+                return KoalAvr
+        }
+        console.log(avrKoal(88,91,110));
+
+        */
+
+        function average (a, b, c) {
+                return( a + b + c) / 3
+        }
+
+        const avgDolphin = average(96, 108, 89)
+        console.log(avgDolphin)
+        const avgKoala = average(88, 91, 110) 
+        console.log(avgKoala)
+
+        function findWinner(team1, team2){
+if (team1 > team2){
+        return `The winner is Dolphin`
+}else{
+        return `The winner is Koala`
+        }
+
+        }
+
+        console.log(findWinner(avgDolphin, avgKoala))
+
