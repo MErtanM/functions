d5bb1bc49f580e683c4299fee8cec75734269c69
MErtanM <mertan1971@gmail.com>
2021-12-12 21:21:31 +0300

    no commit

--- functions.js
+++ functions.js
@@ -21,3 +21,35 @@ function juiceExtractor (fruit1, fruit2){
 console.log(juiceExtractor("apple", "orange"))
 const myJuice = juiceExtractor("apple", "orange")
 console.log(myJuice)
+
+/*
+function carFactory (car1, car2){
+        const car = `This is ${car1} and ${car2} car.`
+       console.log(car)
+       return car
+}
+console.log(carFactory("mercedes", "opel"))  
+const myCar =carFactory("mercedes", "opel")
+console.log(myCar)
+*/
+
+// funtion tanimlama yontemi
+// 1 funtion declaration
+
+function calcAge(birthyear){
+    
+        //const age =2021 - birthyear
+       // return age
+       return 2021 - birthyear
+}
+
+console.log(calcAge(1997))
+
+
+// 2 Function Expression code
+
+const addition = function(a, b){
+        return a + b        
+}
+const total =addition(1965, 1612775)
+console.log(total)
