2da72dc7703dc8da6e3745bf4333a1278b1c0d18
MErtanM <mertan1971@gmail.com>
2021-12-13 22:21:36 +0300

    no change

--- functions.js
+++ functions.js
@@ -100,7 +100,7 @@ console.log(avrDolp(96,108,89));
         console.log(avrKoal(88,91,110));
 
         */
-
+/*
         function average (a, b, c) {
                 return( a + b + c) / 3
         }
@@ -120,4 +120,63 @@ if (team1 > team2){
         }
 
         console.log(findWinner(avgDolphin, avgKoala))
+
+*/
+
+     /*
+     QUESTION
+     Find out if a given year is a “leap” year.
+In the Gregorian calendar, three criteria must be taken into account to identify leap years:
+1-The year must be evenly divisible by 4;
+2-If the year can also be evenly divided by 100, it is not a leap year; unless...
+3-The year is also evenly divisible by 400. Then it is a leap year.
+According to these rules, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300, and 2500 are not the leap years.
+Write a program that takes the year from user and gives output as “2020 is a leap year” or “2019 is not a leap year” .
+    
+      
+    
+     */
+/*
+const year =prompt("Enter a year: ")
+        function leapYear (year){
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400== 0){
+return `${year} is a leap year.`
+
+        }else { 
+                return`${year} is not a leap year.`
+        }
+        }
+        leapYear()
+        console.log(leapYear(year))
+*/
+        
+
+        /*
+        1.Take a positive number from user,
+2. Write a function that check whether  the number is "Armstrong" or not.
+
+An n-digit number that is the sum of the nth powers of its digits is called an n-Armstrong number. Examples :
+371 =3 ** 3 + 7 ** 3 + 1 ** 3;
+9474 = 9 ** 4 + 4 ** 4 + 7 ** 4 + 4 ** 4;
+93084 = 9 ** 5 + 3 ** 5 + 0 ** 5 + 8 ** 5 + 4 **5.
+
+P.S:  consider the negative, decimal and any entries other than numeric values then display a warning message to the user.
+*/
+
+
+const number = prompt("Enter a three digit number: ")
+
+        function isArmstrong(number){
+        const len = number.lenght
+        const newNumber = number.split('')
+        console.log(newNumber)
+        console.log(len)  
+
+        const sum = newNumber[0] ** len + newNumber[1] ** len + newNumber[2] ** len
+
+        return sum == number ? "is Armstrong" : "not Armstrong"
+        
+        }
+        console.log(isArmstrong(number))
+
 
